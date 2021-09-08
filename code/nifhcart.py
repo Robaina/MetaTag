c47f2ec2734878e72b97b40b99d978e3a03f2e27
Semidán <semidan.robaina@gmail.com>
2021-09-08 08:33:09 +0100

    nifH

--- code/nifhcart.py
+++ code/nifhcart.py
@@ -2,7 +2,7 @@
 Classify nifH sequences in clusters based on CART model,
 CART model from: https://sfamjournals.onlinelibrary.wiley.com/doi/10.1111/1758-2229.12455
 
-Usage: python3 nifhcart.py input_fasta [output_fasta]
+Usage: python3 nifhcart.py input_fasta input_alignment [output_fasta]
 """
 
 import os
@@ -14,29 +14,46 @@ def getnifHclusterID(seq: list) -> str:
     """
     Assign cluster to nifH sequence based on CART model in
     https://sfamjournals.onlinelibrary.wiley.com/doi/10.1111/1758-2229.12455
+
+    NOTE: Adding 85 to original sequence position (Azotobacter vinelandii) to
+    correct for alignment displacement (mind 0-indexing in python)
     """
     CART = {
-        109: ['F', 'W', 'Y'],
-        49: ['A', 'D', 'I'],
-        53: ['L', 'M', 'W']
+        193: ['F', 'W', 'Y'],
+        133: ['A', 'D', 'I'],
+        137: ['L', 'M', 'W']
     }
     aa_pos = list(CART.keys())
     if len(seq) < max(aa_pos):
         return '0'
-    if seq[aa_pos[0] - 1] in CART[aa_pos[0]]:
+    if seq[aa_pos[0]] in CART[aa_pos[0]]:
         return 'I'
-    elif seq[aa_pos[1] - 1] in CART[aa_pos[1]]:
+    elif seq[aa_pos[1]] in CART[aa_pos[1]]:
         return 'II'
-    elif seq[aa_pos[2] - 1] in CART[aa_pos[2]]:
+    elif seq[aa_pos[2]] in CART[aa_pos[2]]:
         return 'III'
     else:
         return 'IV'
 
-def addClusterToNifH(input_fasta: str, output_fasta: str = None) -> None:
+def getRecordAlignments(input_alignment: str) -> dict:
+    """
+    Get dict of lists containing sequence alignments
+    """
+    with open(input_alignment) as inalign:
+        return {
+            record.id.split('_')[0]: list(record.seq)
+            for record in SeqIO.parse(inalign, 'fasta')
+        }
+
+def addClusterToNifH(input_fasta: str, input_alignment: str,
+                     output_fasta: str = None) -> None:
     """
     Add assigned cluster to nifH sequences in fasta file
     """
     input_fasta = os.path.abspath(input_fasta)
+    input_alignment = os.path.abspath(input_alignment)
+    recordAligns = getRecordAlignments(input_alignment)
+
     if output_fasta is None:
         base, ext = os.path.splitext(input_fasta)
         output_fasta = f'{base}_clustered{ext}'
@@ -45,7 +62,8 @@ def addClusterToNifH(input_fasta: str, output_fasta: str = None) -> None:
 
     with open(input_fasta) as infasta, open(output_fasta, 'w') as outfasta:
         for record in SeqIO.parse(infasta, 'fasta'):
-            cluster_id = getnifHclusterID(list(record.seq))
+            record_align_seq = recordAligns[record.id.split('_')[0]]
+            cluster_id = getnifHclusterID(record_align_seq)
             record.id = f'{record.id}_cluster_{cluster_id}'
             record.name = ''
             record.description = ''
@@ -55,10 +73,11 @@ def addClusterToNifH(input_fasta: str, output_fasta: str = None) -> None:
 if __name__ == '__main__':
 
     input_fasta = sys.argv[1]
-    if len(sys.argv) > 2:
-        output_fasta = sys.argv[2]
+    input_alignment = sys.argv[2]
+    if len(sys.argv) > 3:
+        output_fasta = sys.argv[3]
     else:
         output_fasta = None
 
-    addClusterToNifH(input_fasta, output_fasta)
+    addClusterToNifH(input_fasta, input_alignment, output_fasta)
     
