3fc47da11cadbe89e1fb943c52239c198b47740f
Semidán <semidan.robaina@gmail.com>
2021-09-17 10:51:13 +0100

    refactor

--- code/test.py
+++ code/test.py
@@ -1,12 +1,28 @@
-from phyloplacement.database import filterFASTAByHMM
+from phyloplacement.database import filterFASTAByHMM, removeDuplicatesFromFastaByID
 
 
 if __name__ == '__main__':
     
-    hmm = '/home/robaina/hmmer/narGTIGR01580.1.HMM'
-    ifile = '/home/robaina/cleangenomes/results/Marref_V6.fasta'
+    # hmm = '/home/robaina/hmmer/narGTIGR01580.1.HMM'
+    # # ifile = '/home/robaina/cleangenomes/results/Marref_V6.fasta'
+    # ifile = '/home/robaina/Documents/MAR_database/mardb_proteins_V6.faa'
+
+    # filterFASTAByHMM(hmm_model=hmm,
+    #                  input_fasta=ifile,
+    #                  output_fasta=None,
+    #                  method='hmmsearch')
+
+    # input_fasta = '/home/robaina/Documents/MAR_database/mardb_proteins_V6.faa'
+    
+    hmm = '../data/hmms/narGTIGR01580.1.HMM'
+    input_fasta = '/usr/gonzalez/metagenomes/MarPeptides/mardb_proteins_V6.fasta'
+    input_fasta_no_dup = '/usr/gonzalez/metagenomes/MarPeptides/mardb_proteins_V6_no_duplicates.fasta'
+    
+    removeDuplicatesFromFastaByID(input_fasta, output_fasta=input_fasta_no_dup) 
 
     filterFASTAByHMM(hmm_model=hmm,
-                     input_fasta=ifile,
+                     input_fasta=input_fasta_no_dup,
                      output_fasta=None,
                      method='hmmsearch')
+
+# ValueError: The ID or alternative IDs of Hit 'WP_113256187.1_MMP09508687' exists in this QueryResult
