e71f3528993946b9cb7c12aab0d44df65ab08853
gonzalez <gonzalez@pegaso.microbiologia.ull.es>
2021-09-17 12:51:00 +0100

    filter duplicates

--- code/test.py
+++ code/test.py
@@ -1,24 +1,15 @@
-from phyloplacement.database import filterFASTAByHMM, removeDuplicatesFromFastaByID
+from phyloplacement.database import filterFASTAByHMM, removeDuplicatesFromFasta
 
 
 if __name__ == '__main__':
-    
-    # hmm = '/home/robaina/hmmer/narGTIGR01580.1.HMM'
-    # # ifile = '/home/robaina/cleangenomes/results/Marref_V6.fasta'
-    # ifile = '/home/robaina/Documents/MAR_database/mardb_proteins_V6.faa'
-
-    # filterFASTAByHMM(hmm_model=hmm,
-    #                  input_fasta=ifile,
-    #                  output_fasta=None,
-    #                  method='hmmsearch')
-
+        
+    hmm = 'data/hmms/narGTIGR01580.1.HMM'
     # input_fasta = '/home/robaina/Documents/MAR_database/mardb_proteins_V6.faa'
-    
-    hmm = '../data/hmms/narGTIGR01580.1.HMM'
     input_fasta = '/usr/gonzalez/metagenomes/MarPeptides/mardb_proteins_V6.fasta'
     input_fasta_no_dup = '/usr/gonzalez/metagenomes/MarPeptides/mardb_proteins_V6_no_duplicates.fasta'
     
-    removeDuplicatesFromFastaByID(input_fasta, output_fasta=input_fasta_no_dup) 
+    # removeDuplicatesFromFastaByID(input_fasta, output_fasta=input_fasta_no_dup) 
+    # removeDuplicatesFromFasta(input_fasta, output_fasta=input_fasta_no_dup)
 
     filterFASTAByHMM(hmm_model=hmm,
                      input_fasta=input_fasta_no_dup,
