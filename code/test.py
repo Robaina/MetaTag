90c9487ba3276aa9b6a678a11896b687e143cc10
Semidán <semidan.robaina@gmail.com>
2021-09-18 16:09:03 +0100

    iqtree

--- code/test.py
+++ code/test.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# conda activate traits
+
 from phyloplacement.database import filterFASTAByHMM, removeDuplicatesFromFasta
 from phyloplacement.phylotree import (runMuscle, runTrimal,
-                                      convertFastaAlnToPhylip, runFastTree)
+                                      convertFastaAlnToPhylip, runFastTree,
+                                      runIqTree)
 
 if __name__ == '__main__':
         
@@ -23,33 +27,9 @@ if __name__ == '__main__':
     # runTrimal(input_aln='data/nxr/mardb_proteins_V6_TIGR015180.1.fasta.aln',
     #           output_aln=None)
 
-    convertFastaAlnToPhylip(input_fasta_aln='data/nxr/mardb_proteins_V6_TIGR015180.1.fasta.aln',
-                            output_file='data/nxr/mardb_proteins_V6_TIGR015180.1.phylip')
+    # convertFastaAlnToPhylip(input_fasta_aln='data/nxr/mardb_proteins_V6_TIGR015180.1.fasta.aln',
+    #                         output_file='data/nxr/mardb_proteins_V6_TIGR015180.1.phylip')
     
-    runFastTree(input_phylip='data/nxr/mardb_proteins_V6_TIGR015180.1.phylip')
+    # runFastTree(input_algns='data/nxr/mardb_proteins_V6_TIGR015180.1.fasta_trimal.aln')
 
-
-
-"""
-NOTE: producing this error during phylip conversion
-
-Traceback (most recent call last):
-  File "/home/robaina/miniconda3/envs/traits/lib/python3.9/site-packages/Bio/File.py", line 72, in as_handle
-    with open(handleish, mode, **kwargs) as fp:
-TypeError: expected str, bytes or os.PathLike object, not TextIOWrapper
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/home/robaina/Documents/TRAITS/code/accesory/fasta_to_phylip.py", line 34, in <module>
-    main()
-  File "/home/robaina/Documents/TRAITS/code/accesory/fasta_to_phylip.py", line 30, in main
-    AlignIO.write(records, output_handle, "phylip")
-  File "/home/robaina/miniconda3/envs/traits/lib/python3.9/site-packages/Bio/AlignIO/__init__.py", line 215, in write
-    count = writer_class(fp).write_file(alignments)
-  File "/home/robaina/miniconda3/envs/traits/lib/python3.9/site-packages/Bio/AlignIO/Interfaces.py", line 129, in write_file
-    self.write_alignment(alignment)
-  File "/home/robaina/miniconda3/envs/traits/lib/python3.9/site-packages/Bio/AlignIO/PhylipIO.py", line 101, in write_alignment
-    raise ValueError(
-ValueError: Repeated name 'GCA_003230' (originally 'GCA_003230455.1_01774_MMP09240198'), possibly due to truncation
-"""
+    runIqTree(input_algns='data/nxr/mardb_proteins_V6_TIGR015180.1.fasta_trimal.aln')
