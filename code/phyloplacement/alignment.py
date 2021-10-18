a7431b44c49493b70a98b7179d9f256323445c29
Semidán <semidan.robaina@gmail.com>
2021-10-18 14:04:50 +0100

    tempfile

--- code/phyloplacement/alignment.py
+++ code/phyloplacement/alignment.py
@@ -3,10 +3,9 @@ Tools to perform multiple sequence alignments
 """
 
 import os
-import tempfile
 
 import phyloplacement.wrappers as wrappers
-from phyloplacement.utils import setDefaultOutputPath
+from phyloplacement.utils import createTemporaryFilePath, setDefaultOutputPath
 from phyloplacement.database.manipulation import (convertStockholmToFastaAln,
                                                   convertFastaAlnToPhylip,
                                                   convertPhylipToFastaAln)
@@ -58,26 +57,33 @@ def alignShortReadsToReferenceMSA(ref_msa: str, query_seqs: str,
     )
     
     if method.lower() in 'hmmalign':
+        temp_file_path = createTemporaryFilePath()
+
         wrappers.runHMMbuild(input_aln=ref_msa,
                              output_hmm=output_hmm)
         
-        with tempfile.NamedTemporaryFile(mode='w') as tempstock:
-            wrappers.runHMMalign(input_aln=ref_msa,
-                                 input_hmm=output_hmm,
-                                 input_seqs=query_seqs,
-                                 output_aln_seqs=tempstock.name)
-            convertStockholmToFastaAln(input_stockholm=tempstock.name,
-                                       output_fasta=output_aln_seqs)
+        wrappers.runHMMalign(input_aln=ref_msa,
+                                input_hmm=output_hmm,
+                                input_seqs=query_seqs,
+                                output_aln_seqs=temp_file_path)
+        convertStockholmToFastaAln(input_stockholm=temp_file_path,
+                                    output_fasta=output_aln_seqs)
+        os.remove(temp_file_path)
+
     elif method.lower() in 'papara':
-        with tempfile.NamedTemporaryFile(mode='w') as tempphy, \
-             tempfile.NamedTemporaryFile(mode='w') as tempqueryaln:
-             convertFastaAlnToPhylip(input_fasta_aln=output_aln_seqs,
-                                     output_file=tempphy.name)
-             wrappers.runPapara(tree_nwk=tree_nwk,
-                                msa_phy=tempphy.name,
-                                query_fasta=query_seqs,
-                                output_aln=tempqueryaln.name)
-             convertPhylipToFastaAln(input_phylip=tempqueryaln.name,
-                                     output_file=output_aln_seqs)
+        temp_phy_path = createTemporaryFilePath()
+        temp_aln_path = createTemporaryFilePath()
+
+        convertFastaAlnToPhylip(input_fasta_aln=output_aln_seqs,
+                                output_file=temp_phy_path)
+        wrappers.runPapara(tree_nwk=tree_nwk,
+                           msa_phy=temp_phy_path,
+                           query_fasta=query_seqs,
+                           output_aln=temp_aln_path)
+        convertPhylipToFastaAln(input_phylip=temp_aln_path,
+                                output_file=output_aln_seqs)
+        
+        os.remove(temp_phy_path)
+        os.remove(temp_aln_path)
     else:
         raise ValueError('Alignment method not implemented')
