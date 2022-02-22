cb07725e089337212e3b4ccc3d2abde4daa2d165
Semidán <semidan.robaina@gmail.com>
2022-02-22 11:51:58 +0000

    update nifH

--- genes/nifH/nifH_pipeline.sh
+++ genes/nifH/nifH_pipeline.sh
@@ -4,14 +4,10 @@
 #                           nifH pipeline                           #
 # ***************************************************************** #
 
-# Preprocess
-python3 ./code/preprocess.py \
- --in data/MAR_HQ \
- --outfile data/marhq_cleaned.faa
 
 # Make database
 python3 ./code/makedatabase.py \
- --in data/marhq_cleaned.faa \
+ --in data/final_ref_database.fasta \
  --outdir genes/nifH/results/ \
  --hmm genes/nifH/hmms/TIGR01287.1.HMM \
  --max_size 800 \
@@ -27,7 +23,7 @@ python3 ./code/buildtree.py \
 
 # Remove tree branch outliers
 python3 ./code/removetreeoutliers.py \
- --tree genes/nifH/results/ref_database.contree \
+ --tree genes/nifH/results/ref_database.newick \
  --outdir genes/nifH/results/ \
  --aln genes/nifH/results/ref_database.faln
 
@@ -40,14 +36,14 @@ python3 ./code/classifyNifHsequences.py \
 
 # Relabel reference tree and msa
 python3 ./code/relabeltree.py \
- --tree genes/nifH/results/ref_database_shrink.contree \
+ --tree genes/nifH/results/ref_database_shrink.newick \
  --aln genes/nifH/results/ref_database.faln \
  --labels genes/nifH/results/ref_database_id_dict_clustered.pickle \
 
- # Commit to GitHub
-# git add .
-# git commit -m "Add nifH results, clustered"
-# git push origin main
+# Commit to GitHub
+git add .
+git commit -m "Add nifH results, clustered"
+git push origin main
 
 # Send notification
-# python3 ./code/notify.py --klink https://github.com/Robaina/TRAITS/
+python3 ./code/notify.py --klink https://github.com/Robaina/TRAITS/
