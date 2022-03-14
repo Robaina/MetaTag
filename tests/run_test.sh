821ce410d4c94093ea9e4a8caaaed44696a8c0b5
Semidán Robaina Estévez <semidan.robaina@gmail.com>
2022-03-14 10:06:51 +0000

    Enable evaluation of multiple taxonomic levels at the same time in countplacements.py

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -16,53 +16,53 @@
 #                                                                                        #
 # ************************************************************************************** #
 
-rm -r tests/test_results; mkdir tests/test_results; mkdir tests/test_results/gappa/
+# rm -r tests/test_results; mkdir tests/test_results; mkdir tests/test_results/gappa/
 
-# Preprocess
-python3 code/preprocess.py \
- --in tests/test_data/database/ \
- --outfile tests/test_results/test_data_cleaned.faa \
- --export-duplicates
+# # Preprocess
+# python3 code/preprocess.py \
+#  --in tests/test_data/database/ \
+#  --outfile tests/test_results/test_data_cleaned.faa \
+#  --export-duplicates
 
-# Make database
-python3 code/makedatabase.py \
- --in tests/test_results/test_data_cleaned.faa \
- --outdir tests/test_results \
- --hmms tests/test_data/TIGR01287.1.HMM \
-        tests/test_data/TIGR02016.1.HMM \
- --max_sizes 20 5 \
- --min_seq_length 10 --max_seq_length 2000 \
- --relabel_prefixes "ref_" "out_" \
- --relabel
+# # Make database
+# python3 code/makedatabase.py \
+#  --in tests/test_results/test_data_cleaned.faa \
+#  --outdir tests/test_results \
+#  --hmms tests/test_data/TIGR01287.1.HMM \
+#         tests/test_data/TIGR02016.1.HMM \
+#  --max_sizes 20 5 \
+#  --min_seq_length 10 --max_seq_length 2000 \
+#  --relabel_prefixes "ref_" "out_" \
+#  --relabel
 
-# Alignment and tree
-python3 code/buildtree.py \
- --in tests/test_results/ref_database.faa \
- --outdir tests/test_results/ \
- --msa_method "muscle" \
- --tree_model "iqtest" \
- --tree_method "iqtree"
+# # Alignment and tree
+# python3 code/buildtree.py \
+#  --in tests/test_results/ref_database.faa \
+#  --outdir tests/test_results/ \
+#  --msa_method "muscle" \
+#  --tree_model "iqtest" \
+#  --tree_method "iqtree"
 
-# Remove tree branch outliers
-python3 code/removetreeoutliers.py \
- --tree tests/test_results/ref_database.newick \
- --outdir tests/test_results/ \
- --aln tests/test_results/ref_database.faln
+# # Remove tree branch outliers
+# python3 code/removetreeoutliers.py \
+#  --tree tests/test_results/ref_database.newick \
+#  --outdir tests/test_results/ \
+#  --aln tests/test_results/ref_database.faln
 
-# Preprocess query sequences
-python3 code/preprocess.py \
- --in tests/test_data/query.faa \
- --outfile tests/test_results/query_cleaned.faa \
- --idprefix "query_" --relabel
+# # Preprocess query sequences
+# python3 code/preprocess.py \
+#  --in tests/test_data/query.faa \
+#  --outfile tests/test_results/query_cleaned.faa \
+#  --idprefix "query_" --relabel
 
-# Place query sequences
-python3 code/placesequences.py \
- --aln tests/test_results/ref_database_shrink.faln \
- --tree tests/test_results/ref_database_shrink.newick \
- --query tests/test_results/query_cleaned.faa \
- --outdir tests/test_results/ \
- --aln_method "papara" \
---tree_model tests/test_results/ref_database.log
+# # Place query sequences
+# python3 code/placesequences.py \
+#  --aln tests/test_results/ref_database_shrink.faln \
+#  --tree tests/test_results/ref_database_shrink.newick \
+#  --query tests/test_results/query_cleaned.faa \
+#  --outdir tests/test_results/ \
+#  --aln_method "papara" \
+# --tree_model tests/test_results/ref_database.log
 
 # Assign taxonomy to placed sequences
 python3 code/labelplacements.py \
@@ -77,11 +77,10 @@ python3 code/labelplacements.py \
 # Count placements (filter by taxon, cluster id and quality score)
 python3 code/countplacements.py \
  --taxtable tests/test_results/gappa/placed_tax_assignments.tsv \
- --taxlevel "family" \
+ --taxlevels "genus" "family" "class" "order" \
  --cluster_ids "G1" "G2" \
  --score_threshold 0.6 \
- --outfile tests/test_results/gappa/placed_family_tax_counts.tsv \
- --outpdf tests/test_results/gappa/placed_family_tax_counts.pdf
+ --outdir tests/test_results/gappa/
 
 # Relabel tree and alignment
 python3 code/relabeltree.py \
