7b15aff0db121602ce5d463d77f67f3574023ef8
Semidán <semidan.robaina@gmail.com>
2022-06-21 12:44:23 +0100

    updated squeeze parser

--- tests/squeeze_test.sh
+++ tests/squeeze_test.sh
@@ -4,3 +4,9 @@ python3 code/squeezemodify.py \
  --squeeze_table tests/test_data/squeeze_out_example.txt \
  --taxplacement_table tests/test_data/placement_example.tsv \
  --outfile tests/test_data/squeeze_out_example_replaced.txt
+
+
+python3 code/squeezemodify.py \
+ --squeeze_table /home/robaina/Documents/TRAITS/08.artico022_DNA.fun3.tax.noidfilter.wranks \
+ --taxplacement_table tests/test_data/placement_example.tsv \
+#  --outfile tests/test_data/squeeze_out_example_replaced.txt
