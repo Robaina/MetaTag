2ab4dd12d7def89e4535adb2e942f79ac058ec45
Semidán Robaina Estévez <semidan.robaina@gmail.com>
2022-06-23 13:41:11 +0100

    update nifH

--- genes/nifH/nifH_pipeline.sh
+++ genes/nifH/nifH_pipeline.sh
@@ -37,9 +37,29 @@ python3 ./code/classifyNifHsequences.py \
  --outdict genes/nifH/results/ref_database_id_dict_clustered.pickle \
  --out_clusters_file genes/nifH/data/clusters.tsv
 
-# Relabel reference tree and msa
-python3 ./code/relabeltree.py \
- --tree genes/nifH/results/ref_database.newick \
- --aln genes/nifH/results/ref_database.faln \
+# Assign taxonomy to placed sequences
+python3 code/labelplacements.py \
+ --jplace genes/nifH/envdata/epa_result.jplace \
  --labels genes/nifH/results/ref_database_id_dict_clustered.pickle \
+ --ref_clusters genes/nifH/data/clusters.tsv \
+ --ref_cluster_scores genes/nifH/data/cluster_scores.tsv \
+ --outgroup "out_" \
+ --prefix "placed_tax_" \
+ --outdir genes/nifH/envdata/
+
+# Count placements (filter by taxon, cluster id and quality score)
+python3 code/countplacements.py \
+ --taxtable genes/nifH/envdata/placed_tax_assignments.tsv \
+ --taxlevels "order" "class" "family" "genus" \
+ --cluster_ids "cluster_I" "cluster_II" "cluster_III" \
+ --score_threshold 0.5 \
+ --outdir genes/nifH/envdata/
+
+# Relabel tree and alignment
+python3 code/relabeltree.py \
+ --tree genes/nifH/envdata/epa_result.newick \
+ --labels genes/nifH/results/ref_database_id_dict_clustered.pickle \
+          genes/nifH/envdata/query_cleaned_id_dict.pickle \
+ --taxonomy \
+ --label_prefixes "ref_" "query_"
 
