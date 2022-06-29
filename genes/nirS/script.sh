37a8246f7e6905b77caeeb15eea5ec03e11c0a2a
Semidán Robaina Estévez <semidan.robaina@gmail.com>
2022-06-29 11:03:23 +0100

    nirS

---
+++ genes/nirS/script.sh
@@ -0,0 +1,11 @@
+python3 ./code/makedatabase.py \
+ --in data/final_ref_database.fasta \
+ --outdir genes/nirS/results \
+ --hmms genes/nirS/hmms/K15864.hmm \
+        genes/nirS/hmms/PF02239.hmm \
+ --hmmsearch_args ” -T 413.67, None” \
+ --prefix “nirS_DB_” \
+ --relabel \
+ --relabel_prefixes “refKO_” “refPF_” \
+ --remove_duplicates \
+ --max_sizes 800
